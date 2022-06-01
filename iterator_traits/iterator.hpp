a8a0e58d1e3b6a4abebfdc77256bc72fbb6dec6b
Aleksandrs marks Krusts <alkrusts@f1r4s20.codam.nl>
2022-06-01 20:46:49 +0200

    figured out iterators

--- iterator_traits/iterator.hpp
+++ iterator_traits/iterator.hpp
@@ -6,26 +6,29 @@
 /*   By: alkrusts <alkrusts@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/31 16:11:23 by alkrusts      #+#    #+#                 */
-/*   Updated: 2022/06/01 18:15:12 by alkrusts      ########   odam.nl         */
+/*   Updated: 2022/06/01 20:18:09 by alkrusts      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iterator_traits.hpp"
+#include "iterator_tags.hpp"
 
 namespace ft
 {
 	template <class T>
-	class Iterator : public ft::iterator<std::input_iterator_tag, T>
+	class Iterator : public ft::iterator<ft::input_iterator_tag, T>
 	{
 		T		*p;
 
 		public:
 			Iterator(T* x) :p(x) {}
+
 			Iterator(const Iterator& mit) : p(mit.p) {}
-			Iterator& operator++() {++p;return *this;}
-			Iterator operator++(int) {Iterator tmp(*this); operator++(); return tmp;}
-			bool operator==(const Iterator& rhs) const {return p==rhs.p;}
-			bool operator!=(const Iterator& rhs) const {return p!=rhs.p;}
-			T& operator*() {return *p;}
+
+			Iterator	&operator++() {++p;return *this;}
+			Iterator	operator++(int) {Iterator tmp(*this); operator++(); return tmp;}
+			bool		operator==(const Iterator& rhs) const {return p==rhs.p;}
+			bool		operator!=(const Iterator& rhs) const {return p!=rhs.p;}
+			T			&operator*() {return *p;}
 	}; 
 }
--- iterator_traits/iterator_tags.hpp
+++ iterator_traits/iterator_tags.hpp
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   test.cpp                                           :+:    :+:            */
+/*   iterator_tags.hpp                                  :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: alkrusts <alkrusts@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/05/31 16:11:29 by alkrusts      #+#    #+#                 */
-/*   Updated: 2022/06/01 20:19:07 by alkrusts      ########   odam.nl         */
+/*   Created: 2022/06/01 20:19:29 by alkrusts      #+#    #+#                 */
+/*   Updated: 2022/06/01 20:19:30 by alkrusts      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iterator.hpp"
-#include <string>
-#include <iostream>
-
-int main () {
-std::string numbers[]={"Hallo", "waza", "'nye", "sadf", "BYEEEE"};
-	ft::Iterator <std::string>from(numbers);
-	ft::Iterator <std::string>until(numbers+5);
-  for (ft::Iterator <std::string>it=from; it!=until; it++)
-	  std::cout << *it << ' ';
-  std::cout << '\n';
-
-  return 0;
+#ifndef ITERATOR_TAGS_HPP
+# define ITERATOR_TAGS_HPP
+namespace ft
+{
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag       : public input_iterator_tag {};
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
+	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 }
+#endif
